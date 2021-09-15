fc8f142e1436953f83176e179bd95781d1c81c43
Danil Kovtonyuk <danx.kov@gmail.com>
2021-09-15 17:58:21 +1000

    remove instance

--- instances.js
+++ instances.js
@@ -1,13 +1,6 @@
 module.exports = [
   {
     isETH: true,
-    denomination: '100000000000000000',
-    domain: 'avax-01.tornadocash.eth',
-    symbol: 'AVAX',
-    decimals: 18,
-  },
-  {
-    isETH: true,
     denomination: '10000000000000000000',
     domain: 'avax-10.tornadocash.eth',
     symbol: 'AVAX',
